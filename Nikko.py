03fcc1fbf133d17a04d3baf9f8418a12bb2d1821
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2022-02-05 15:04:46 +0530

    Update Nikko.py

--- Nikko.py
+++ Nikko.py
@@ -37,6 +37,14 @@ async def lunaQuery(query: str, user_id: int):
             await arq.translate(resp, LANGUAGE)
         ).result.translatedText
     )
+async def type_and_send(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(2))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
 
 async def type_and_send(message):
     chat_id = message.chat.id
